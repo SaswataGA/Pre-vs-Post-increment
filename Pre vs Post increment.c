3795b9c21ff1c3e23c80488788d31d6c2255b5d6
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-07-28 10:59:19 +0600

    Add files via upload

---
+++ Pre vs Post increment.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main ()
+//++i = pre icrement
+//i++ = post increment
+{
+    int i=0;
+
+    for(i=0; i<=10; i++)
+
+        printf(" %d ",i);
+
+    printf("\n");
+
+
+    for(i=0; i<=10; ++i)
+
+        printf(" %d ",i);
+
+    printf("\n");
+
+
+
+
+}
